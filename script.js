58d5db31a97946280f681ca09868cd230682be6b
Beggardefi <beggardefi@gmail.com>
2025-05-24 19:31:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-// Minimal test for Web3Modal and Ethers.js
 alert("DApp JS Loaded!");
 
-console.log("window.Web3Modal:", window.Web3Modal);
-console.log("window.ethers:", window.ethers);
+if (typeof window.Web3Modal !== "undefined") {
+  alert("Web3Modal is loaded!");
+} else {
+  alert("Web3Modal is NOT loaded!");
+}
 
-const projectId = "9bb77bfd32a850e43324d0b8c8ff41dc";
-const chains = [{
-  chainId: 56,
-  name: "Binance Smart Chain",
-  rpcUrl: "https://bsc-dataseed.binance.org/"
-}];
-const metadata = {
-  name: "BHIKX Test DApp",
-  description: "Testing WalletConnect",
-  url: "https://beggardefi.github.io/BHIXMETAEWEB/",
-  icons: []
-};
-
-const web3Modal = new window.Web3Modal({
-  projectId,
-  walletConnectVersion: 2,
-  themeMode: "dark",
-  chains,
-  metadata
-});
-
-document.getElementById("connectBtn")?.addEventListener("click", async () => {
-  alert("Connect Button Clicked!");
-  try {
-    const ethereumProvider = await web3Modal.connect();
-    const provider = new window.ethers.providers.Web3Provider(ethereumProvider);
-    const signer = provider.getSigner();
-    const address = await signer.getAddress();
-    alert("Wallet Connected: " + address);
-  } catch (err) {
-    alert("Error: " + err.message);
-  }
-});
+if (typeof window.ethers !== "undefined") {
+  alert("Ethers.js is loaded!");
+} else {
+  alert("Ethers.js is NOT loaded!");
+}
