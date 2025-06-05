48e291af78a434e9f6e03cf64bc3c0a7847ecee9
Beggardefi <beggardefi@gmail.com>
2025-06-05 14:27:32 +0530

    Create jobportal.js

---
+++ opportunities/jobs/jobportal.js
@@ -0,0 +1,14 @@
+// jobportal.js
+
+async function checkStakingEligibility() {
+  // Placeholder for staking contract logic
+  // Will use Web3 or Ethers.js later
+  // Required Stake: 100 BHIXU
+
+  // Example (Future):
+  // const contract = new ethers.Contract(contractAddress, abi, provider);
+  // const staked = await contract.getStakedAmount(userAddress);
+  // if (staked >= 100) unlockFeatures();
+
+  console.log("Eligibility check logic will be added here once contract is live.");
+}
